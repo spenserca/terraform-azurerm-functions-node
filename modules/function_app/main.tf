40d2eb485829f21e77be531c6395aff88844f712
Spenser Andreassen <spenserca@gmail.com>
2022-06-18 13:51:41 -0500

    fix node version

--- modules/function_app/main.tf
+++ modules/function_app/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_windows_function_app" "function_app" {
     application_insights_key = var.app_insights_key
 
     application_stack {
-      node_version = 14
+      node_version = "~14"
     }
 
     cors {
